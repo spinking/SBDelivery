e64ba1432affa8164d5a6dc6ebe63ca713e7b6c0
Serge Bashkatov <spinking@mail.ru>
2021-02-14 00:07:45 +0400

    added recovery api

--- app/src/main/java/studio/eyesthetics/sbdelivery/data/network/IAuthApi.kt
+++ app/src/main/java/studio/eyesthetics/sbdelivery/data/network/IAuthApi.kt
@@ -3,16 +3,33 @@ package studio.eyesthetics.sbdelivery.data.network
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
-import studio.eyesthetics.sbdelivery.data.models.auth.EnterRequest
-import studio.eyesthetics.sbdelivery.data.models.auth.EnterResponse
-import studio.eyesthetics.sbdelivery.data.models.auth.RefreshTokenRequest
-import studio.eyesthetics.sbdelivery.data.models.auth.RefreshTokenResponse
+import studio.eyesthetics.sbdelivery.data.models.auth.*
 
 interface IAuthApi {
     @POST("auth/login")
     suspend fun login(
         @Body enterRequest: EnterRequest
     ): EnterResponse
+
+    @POST("auth/register")
+    suspend fun register(
+        @Body registrationRequest: RegistrationRequest
+    ): RegistrationResponse
+
+    @POST("auth/recovery/email")
+    suspend fun recoveryByEmail(
+        @Body recoveryEmailRequest: RecoveryEmailRequest
+    )
+
+    @POST("auth/recovery/code")
+    suspend fun checkRecoveryCode(
+        @Body recoveryCodeRequest: RecoveryCodeRequest
+    )
+
+    @POST("auth/recovery/password")
+    suspend fun changePassword(
+        @Body recoveryPasswordRequest: RecoveryPasswordRequest
+    )
 
     @POST("auth/refresh")
     fun refreshToken(
